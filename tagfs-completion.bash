6120b2bb1ef6c2e1ab5cc868a730745c090ece2c
Raghu <rteja.ngcbits@gmail.com>
2022-08-11 11:55:50 +0530

    more context aware tagfs bash completions

--- tagfs-completion.bash
+++ tagfs-completion.bash
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
 
+
 _tagfs_completions()
 {
-  if [ "${#COMP_WORDS[@]}" != "2" ]; then
-	return
+  if [ "${#COMP_WORDS[@]}" == 2 ]; then
+    COMPREPLY=($(compgen -W "init lstags addtags linktags addresource tagresource lsresources help" "${COMP_WORDS[1]}"))
   fi
 
-  COMPREPLY=($(compgen -W "init lstags addtags linktags addresource tagresource lsresources help" "${COMP_WORDS[1]}"))
+  if [ "${#COMP_WORDS[@]}" == 3 ]; then
+
+    if [ "${COMP_WORDS[1]}" == "addresource" ]; then
+      COMPREPLY=($(compgen -W "$(ls)" "${COMP_WORDS[2]}"))
+    fi
+
+    if [ "${COMP_WORDS[1]}" == "tagresource" ]; then
+      COMPREPLY=($(compgen -W "$(ls)" "${COMP_WORDS[2]}"))
+    fi
+  fi
+
+  if [ "${#COMP_WORDS[@]}" == 4 ]; then
+    if [ "${COMP_WORDS[1]}" == "tagresource" ]; then
+      COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[3]}"))
+    fi
+  fi
 }
 
+complete -F _tagfs_completions tagfs
 complete -F _tagfs_completions tagfs.py
 
